5764629751af1ca9d343c37e98803ddc4a2fa5fc
21721462 <21721462@student.uwa.edu.au>
2017-10-06 11:16:02 +0800

    Add files via upload

---
+++ app_server/controllers/projectController.js
@@ -0,0 +1,54 @@
+var Project = require('../models/projectModel');
+
+exports.academicPageGet = function(req, res, next)
+{
+	res.render('academic', {title: 'Academic'});
+}
+
+exports.academicPagePost = function(req, res, next)
+{
+	var ProjectTitle =  req.body.projTitle;
+	
+	var superVisor = req.body.Supervisor1;
+	
+
+	var superVisor2 = req.body.Supervisor2;
+	
+
+	var superVisor3 = req.body.Supervisor3;
+	
+
+	var ProjectCapacity = req.body.capacityProj;
+	
+
+	console.log(req.body);
+
+
+	var ProjectDesc = req.body.projectDescription;
+
+	var ProjectDiscipline = req.body.discipline;
+
+	var ProjectPrereq = req.body.priorSkill;
+
+
+
+	var ProjectUpload = {
+		title : ProjectTitle,
+		supervisor : superVisor,
+		supervisor2 : superVisor2,
+		supervisor3 : superVisor3,
+		capacity : ProjectCapacity,
+		description : ProjectDesc,
+		prerequsites : ProjectPrereq,
+		discipline : ProjectDiscipline
+	}
+
+	var myProject = new Project(ProjectUpload);
+	myProject.save()
+	.then(item => {
+      res.send("Project succesfully Saved to DB");
+    		})
+    .catch(err => {
+      res.status(400).send("Project not save correctly");
+    });
+}
