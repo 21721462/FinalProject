e88afb9f47c5d09bdd1c950c62d3e4f594049d2a
21721462 <21721462@student.uwa.edu.au>
2017-10-08 16:23:00 +0800

    Update projectModel.js

--- app_server/models/projectModel.js
+++ app_server/models/projectModel.js
@@ -1,16 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var projectSchema = mongoose.Schema({
-	title: {type: String, unique: true, required: true},
+var projectSchema = Schema({
+	title: {type: String, unique: false, required: true},
 	supervisor: {type: String, unique: false, required:true},
 	supervisor2: {type: String, unique: false, required:false},
 	supervisor3: {type: String, unique: false, required:false},
-	capacity: {type: Number, unique: false, required:true},
+	capacityMIN: {type: Number, unique: false, required:true},
+	capacityMAX: {type: Number, unique: false, required:true},
 	description: {type: String, unique: false, required: true},
 	prerequsites: {type: String, unique: false, required: false},
-	discipline: [{type: String, unique: false, required: true}]
-
+	discipline: [{type: String, unique: false, required: false}],
 })
 
 
