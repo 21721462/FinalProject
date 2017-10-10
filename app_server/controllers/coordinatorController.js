99ed1f0d2f8a6bb15c86aac593d440b296d04a1f
khuang247 <31398253+khuang247@users.noreply.github.com>
2017-10-10 14:24:14 +0800

    Added algorithm

--- app_server/controllers/coordinatorController.js
+++ app_server/controllers/coordinatorController.js
@@ -9,21 +9,19 @@ var Grid = require('editable-grid')
 $ = require('jquery');
 
 // Display the cooridnator main page
-exports.cooridnatorLogin = function(req, res, next) {
-	res.render('cooridnatorMainPage');
+exports.coordinatorLogin = function(req, res, next) {
+	res.render('coordinatorMainPage');
 }
-
 
 exports.removeStudent = function (req, res) 
 {
 	
 }
 
-exports.createGrid = function(req, res, next){
+exports.createGrid = function(req, res, next)
+{
 	
 }
-
-
 
 exports.editStudent = function (req, res)
 {
@@ -40,5 +38,38 @@ exports.sendEmailIndividual = function (req, res)
 
 }
 
-
-// Display Student Front End
+exports.allocateProjects = function (req, res) {
+	Project.update({}, { $set: { numAllocated: "0" }}, {multi: true}, function (err) {if(err) return handleError(err);}); //DEBUG
+	Student.find({}).sort({'wam' : -1}).exec(function (serr, students) {
+		if(serr) return serr;
+		Project.find({}).exec(function (perr, projects) {
+			if(perr) return perr;
+			//console.log(projects);
+			for(var student of students) {
+				console.log(student.firstName, student.preferences);
+				var allocated = false;
+				for(var pref of student.preferences) {
+					//console.log(student.firstName, pref);
+					for(var project of projects) {
+						if(pref == project.title) {
+							if(project.discipline.includes(student.discipline)) {
+								if(project.numAllocated < project.capacityMAX) {
+									student.assignedProject = project.title; //DEBUG
+									project.numAllocated++;
+									Student.update({_id: student.id}, { $set: { assignedProject: project.title }}, function (err) {if(err) return handleError(err);});
+									Project.update({_id: project.id}, { $set: { numAllocated: project.numAllocated }}, function (err) {if(err) return handleError(err);});
+									console.log("%s was allocated to %s!\n", student.firstName, student.assignedProject); //DEBUG
+									allocated = true;
+									break;
+								} //Check capacity
+								else console.log("Project full!"); //DEBUG
+							} //Discipline match
+						} //Preference match
+					} //Project iterate
+					if(allocated) break;
+				} //Preference iterate
+			} //Student iterate
+		}); //Project find
+	}); //Student find
+}
+// Display Student Front
