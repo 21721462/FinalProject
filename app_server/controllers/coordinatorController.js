b05afd46824de37f2e6a60c421afd6e55dc19018
NickoMitchell <31398249+NickoMitchell@users.noreply.github.com>
2017-10-15 13:02:25 +0800

    Add files via upload
    Updated coordinatorController, it now has remove and save functions working for students/allocation tab

--- app_server/controllers/coordinatorController.js
+++ app_server/controllers/coordinatorController.js
@@ -4,24 +4,30 @@ var Coordinator = require('../models/coordinatorModel');
 var Student = require('../models/studentModel');
 var Project = require('../models/projectModel');
 var validator = require('validator');
-var Student = require('../models/studentModel');
 var Grid = require('editable-grid')
 $ = require('jquery');
 
 // Display the cooridnator main page
 exports.coordinatorLogin = function(req, res, next) {
-	res.render('coordinatorMainPage');
+	Student.find().exec(function(err, studentList){
+		Project.find().exec(function(err, projectList) {
+			res.render('coordinatorFrontEnd',{title: "Coordinator Front End", jsonthing: JSON.stringify(studentList), jsonProject: JSON.stringify(projectList)});
+		});
+	});
 }
+
+
 
 exports.removeStudent = function (req, res) 
 {
 	
 }
 
-exports.createGrid = function(req, res, next)
-{
+exports.createGrid = function(req, res, next){
 	
 }
+
+
 
 exports.editStudent = function (req, res)
 {
@@ -74,4 +80,70 @@ exports.allocateProjects = function (req, res) {
 		}); //Project find
 	}); //Student find
 }
-// Display Student Front
+
+exports.deleteRow = function(req, res){
+	console.log("removed row");
+	var row = req.body.rowID;
+	Student.find({'_id': row}).exec(function(err, doc){
+				console.log(JSON.stringify(doc));
+				//doc.remove();
+			});
+	Student.find({'_id': row}).remove().exec();
+	
+}
+
+
+exports.editRow = function(req, res){
+	var row = req.body.rowID;
+	//Student.find({'_id': row}).remove().exec();
+	console.log("old removed");
+	Student.findOne({'_id': row}, function(err, student){
+		student.firstName = req.body.fName;
+		student.lastName = req.body.lName;
+		student.studentID = req.body.ID;
+		student.assignedProject = req.body.projAlloc;
+		console.log(student);
+		student.save(function(err){
+			if(err){
+			console.log(err);
+		}
+		else console.log("Success");
+		}).then();
+
+				return;
+	});
+// 	var sID = req.body.ID;
+// 	var lNameLocal = req.body.lName;
+// 	var fNameLocal = req.body.fName;
+// 	var projectAllocation = req.body.projAlloc;
+// 	//console.log("variables collected");
+// 	console.log(sID);
+// 	console.log(lNameLocal);
+// 	console.log(fNameLocal);
+// 	var newData = {
+// 		firstName: fNameLocal,
+// 		lastName: lNameLocal,
+// 		studentID: sID
+// 	}
+// 	var toSend = new Student(newData);
+// 	toSend.save(function(err){
+//       if(err){
+//            console.log(err);
+//            return;
+//       }
+
+//       res.json({ token: generateToken(user), user: user });
+// });
+
+	//Student.add({firstName: fNameLocal, lastName: lNameLocal, studentID: sID}).exec();//, assignedProject: projectAllocation})
+	console.log("new row added");
+}
+
+var remove = function(rowNumber){
+	Student.find({'_id': row}).exec(function(err, doc){
+				console.log(JSON.stringify(doc));
+				//doc.remove();
+			});
+	Student.find({'_id': row}).remove().exec();
+}
+// Display Student Front End
